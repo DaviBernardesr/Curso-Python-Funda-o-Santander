8179bf919057e547c7ae7b4742d21aeaa7f57ee3
DaviBernardesr <davibernardesmoto@gmail.com>
2025-09-24 23:08:52 -0300

    operações aritméticas lógicas

---
+++ operações.py
@@ -0,0 +1,66 @@
+
+"""
+Curso de Python Fundação Bradesco
+"""
+
+#Aritméticos
+
+num1 = 10
+num2 = 20
+num3 = 25
+num4 = 50
+
+num10 = 20; num30 = 40
+
+result1 = num1 + num2 #Soma
+result2 = num2 - num3 #Subtração
+result3 = num4 * num10 #Multiplicação
+result5 = num1 / num2 #Divisão
+result6 = num4 // num30 #Divisão Inteira (Número Fechado)
+result7 = num2 % num3 #Resto da Divisão (Mostra o resto da divisão)
+result8 = num3 ** num1 #Potenciação
+
+print(result1)
+print(result2)
+print(result6)
+print(result8)
+
+print(result7)
+
+#Comparação
+#Compara dois valores e retorna True ou False
+
+"""
+== Igual
+!= Diferente
+> Maior
+< Menor
+>= Maior Igual
+<= Menor Igual
+
+Retorna True ou False
+"""
+
+valor1 = 20
+valor2 = 40
+
+igual = valor1 == valor2
+diferente = valor1 != valor2
+maior = valor1 > valor2
+menor = valor1 < valor2
+maiorIgual = valor1 >= valor2
+menorIgual = valor1 <= valor2
+
+print (igual)
+print(menor)
+print(menorIgual)
+
+
+
+#Lógicos
+
+"""
+Os Operadores são: AND, OR, NOT
+"""
+
+#Retorna True ou False
