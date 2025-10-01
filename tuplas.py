16e0a995cc533bce544a1c99e60649d7202d8150
DaviBernardesr <davibernardesmoto@gmail.com>
2025-09-30 23:30:41 -0300

    tuplas

---
+++ tuplas.py
@@ -0,0 +1,17 @@
+"""
+Curso Python Fundação Santander
+"""
+
+#Tuplas em Python é uma "lista" sem ser modificada após criação, sequência de dados ordenados , utiliza () para abrir e fechar.
+# nome_variavel = (elem1, elem2, elem3, elem4).
+
+#tuplas não podem ser modificadas, são heterogêneas.
+
+tupla1 = (1,2,5,6)
+
+tupla2 = (3,6,8)
+
+print(tupla1[2])
+
+print(tupla2[2])
+
