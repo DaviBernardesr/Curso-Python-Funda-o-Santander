c5da1b2ed7dbfd6feeabdffb2753811a6e6957c1
DaviBernardesr <davibernardesmoto@gmail.com>
2025-10-02 23:39:43 -0300

    main

--- main.py
+++ main.py
@@ -3,4 +3,20 @@
 Curso de Python na Fundação Santander
 """
 
+def saudacao():
+    print("Olá func")
+
+
+def retornaPositivo(a, b):
+    if a > 0:
+        print("A é Positivo")
+    if b > 0:
+        print("B é Positivo")
+
+
+#Main
 print ('\nHello World\n')
+
+saudacao()
+
+retornaPositivo(1, 4)
